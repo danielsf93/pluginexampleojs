10ff3aa3683855c914406fba901e8c58fbae7620
PedroCesarAmigo <antunes.amigo@usp.br>
2022-12-07 18:22:14 -0300

    pedro.php com POO

--- pedro.php
+++ pedro.php
@@ -1,3 +1,18 @@
 <?php
 
-echo ("Bananada de goiaba, goiabada de marmelo");
+class Cabelo
+{
+    public $Cor;
+    public $Tamanho;
+}
+
+$cabelo_Talita = new Cabelo;
+$cabelo_Talita->Cor = "Azul";
+$cabelo_Talita->Tamanho = "Grande";
+
+$cabelo_Thiago = new Cabelo;
+$cabelo_Thiago->Cor = "Castanho";
+$cabelo_Thiago->Tamanho = "Medio";
+
+echo("\nO cabelo da Talita é " . $cabelo_Talita->Cor);
+echo("\nO cabelo da Thiago é " . $cabelo_Thiago->Cor);
