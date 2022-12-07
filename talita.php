e7191c1d55f49f05cb418d02ac1172578326857b
TalitaVentura <talitaventura16@outlook.com>
2022-12-07 18:25:07 -0300

    adicionei talita.php

--- talita.php
+++ talita.php
@@ -1,2 +1,14 @@
 <?php
 echo "talita  ventura";
+
+class Instituto
+{
+    public $nome;
+    public $curso;
+}
+
+$fflch = new Instituto;
+$fflch->nome = 'Faculdade de Filosofia Letras e ciências humanas';
+$fflch->curso = 'geografia';
+
+echo "\n A melhor faculdade é a" . $fflch->nome;
